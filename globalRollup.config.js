7494b477e6b63e0eff296c1ee5277edd69ff4bf3
Basile Savouret <basile@smartb.city>
2021-08-18 16:00:53 +0200

    add css plugin to rollup

--- globalRollup.config.js
+++ globalRollup.config.js
@@ -4,6 +4,7 @@ import typescript from "rollup-plugin-typescript2";
 import image from "@rollup/plugin-image";
 import json from "rollup-plugin-json";
 import svgr from "@svgr/rollup";
+import postcss from 'rollup-plugin-postcss'
 
 const getGlobal = (localPackageJson) => {
   const externalsDependencies = Object.keys(
@@ -41,6 +42,7 @@ const getGlobal = (localPackageJson) => {
       image(),
       json(),
       svgr(),
+      postcss()
     ],
   };
 };
