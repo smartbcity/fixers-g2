0b0d7ad88849e059e4678983d787c9c9431c2ab0
basile <basile@smartb.city>
2022-06-30 11:34:19 +0200

    fix

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -1,7 +1,11 @@
 export { AutoCompleteProps, AutoComplete } from './AutoComplete'
 export { CheckBoxProps, CheckBox } from './CheckBox'
 export { DatePickerProps, DatePicker } from './DatePicker'
-export { InputFormProps, InputLabeled as InputForm } from './InputLabeled'
+export {
+  InputLabeledProps,
+  InputLabeled as InputForm,
+  InputLabeledTypes
+} from './InputLabeled'
 export { Option, Select, SelectProps } from './Select'
 export { TextFieldProps, TextField } from './TextField'
 export {
